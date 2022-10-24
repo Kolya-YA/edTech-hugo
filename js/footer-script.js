671a96d362873e7600ff24d7151e14315bf2eb12
Kolya-YA <Kolya-YA@users.noreply.github.com>
2022-10-24 15:34:15 +0000

    deploy: 6007b846154a90946721b9d07ed1b39faf42256a

--- js/footer-script.js
+++ js/footer-script.js
@@ -55,6 +55,48 @@
     });
   });
 
+  // ns-hugo:/home/runner/work/edTech-hugo/edTech-hugo/themes/aittr-wix/assets/js/modules/reasons.js
+  var reasons = document.querySelectorAll(".start-now__item");
+  var selctorsBlock = document.querySelector(".start-now__selectors");
+  var btnsTemplate = document.querySelector("#r-selector");
+  var startNo = 0;
+  reasons.forEach((r, i) => {
+    const btnClone = btnsTemplate.content.cloneNode(true);
+    const btn = btnClone.querySelector("button");
+    btn.dataset.reasonNo = i;
+    if (r.classList.contains("start-now__item--active")) {
+      btn.classList.add("start-now__selector--active");
+      startNo = i;
+    }
+    selctorsBlock.appendChild(btnClone);
+  });
+  var selectors2 = document.querySelectorAll(".start-now__selector");
+  var setActive = (activeNo) => {
+    for (let i = 0; i < reasons.length; i += 1) {
+      reasons[i].className = "";
+      reasons[i].className = activeNo === i ? "start-now__item start-now__item--active" : "start-now__item";
+      selectors2[i].className = "";
+      selectors2[i].className = activeNo === i ? "start-now__selector start-now__selector--active" : "start-now__selector";
+    }
+  };
+  var reasonsRotator = (startFrom) => {
+    let activeNo = startFrom;
+    const chooseItem = () => {
+      activeNo = activeNo + 1 < reasons.length ? activeNo + 1 : 0;
+      setActive(activeNo);
+    };
+    return window.setInterval(chooseItem, 8e3);
+  };
+  var rot = reasonsRotator(startNo);
+  selctorsBlock.addEventListener("click", (event) => {
+    const newStartNo = +event.target.dataset.reasonNo;
+    setActive(newStartNo);
+    clearInterval(rot);
+    rot = reasonsRotator(newStartNo);
+    event.stopPropagation();
+    event.preventDefault();
+  });
+
   // ns-hugo:/home/runner/work/edTech-hugo/edTech-hugo/themes/aittr-wix/assets/js/modules/separator.js
   var separators = document.querySelectorAll(".separator");
   var observer = new IntersectionObserver((entries) => {
